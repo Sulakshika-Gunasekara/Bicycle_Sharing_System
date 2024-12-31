a72f4276a1fa97867217dcef355800e3b1de60eb
Sulakshika-Gunasekara <161403829+Sulakshika-Gunasekara@users.noreply.github.com>
2024-12-31 23:10:39 +0530

    Add new endpoints to get Bike reserve duration

--- src/main/java/dev/mybike/mybike/service/BikeService.java
+++ src/main/java/dev/mybike/mybike/service/BikeService.java
@@ -10,23 +10,23 @@ import java.util.List;
  * and retrieving availability information.
  */
 public interface BikeService {
-    
+
     List<Bike> getAllBikes();
 
     Bike getBikeById(String bikeId);
- 
-    // List<bike> getBikeByStationName(String stationName);
- 
-    Bike trackBike(String bikeId);
- 
- 
-    Bike reportIssueBike(String bikeId, String issue);
- 
-    Bike reserveBike(String bikeId);    //correctly run when only isAvailable is true. otherwise 500 curl error
- 
-    List<Bike> getBikeByIsAvailable(Boolean isAvailabile);
- 
-    Bike updateBikeStation(String bikeId, String oldStationId, String newStationId);
 
+    // List<bike> getBikeByStationName(String stationName);
+
+    Bike trackBike(String bikeId);
+
+    Bike reportIssueBike(String bikeId, String issue);
+
+    Bike reserveBike(String bikeId); // correctly run when only isAvailable is true. otherwise 500 curl error
+
+    Bike returnBike(String bikeId);
+
+    List<Bike> getBikeByIsAvailable(Boolean isAvailabile);
+
+    Bike updateBikeStation(String bikeId, String oldStationId, String newStationId);
 
 }
