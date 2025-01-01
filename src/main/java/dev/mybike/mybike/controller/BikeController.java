b4cf8ec672b6132ed0ea3e65c176d36796054529
Sulakshika-Gunasekara <161403829+Sulakshika-Gunasekara@users.noreply.github.com>
2025-01-01 07:27:33 +0530

    Add services to calculate the trip payment

--- src/main/java/dev/mybike/mybike/controller/BikeController.java
+++ src/main/java/dev/mybike/mybike/controller/BikeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,23 +29,22 @@ import dev.mybike.mybike.service.BikeService;
 @ControllerAdvice
 public class BikeController {
 
-   @Autowired
+    @Autowired
     private BikeService bikeService;
 
-   @GetMapping
-   public ResponseEntity<List<Bike>> getAllBikes() {
-      List<Bike> bikes = bikeService.getAllBikes();
-      return ResponseEntity.ok(bikes);
-   }
+    @GetMapping
+    public ResponseEntity<List<Bike>> getAllBikes() {
+        List<Bike> bikes = bikeService.getAllBikes();
+        return ResponseEntity.ok(bikes);
+    }
 
-   @GetMapping("/{bikeId}")
+    @GetMapping("/{bikeId}")
     public ResponseEntity<Bike> getBikeById(@PathVariable String bikeId) {
-         Bike bike = bikeService.getBikeById(bikeId);
+        Bike bike = bikeService.getBikeById(bikeId);
         return ResponseEntity.ok(bike);
     }
-    
 
-   @GetMapping("/track/{bikeId}")
+    @GetMapping("/track/{bikeId}")
     public ResponseEntity<Bike> trackBike(@PathVariable String bikeId) {
         Bike bike = bikeService.trackBike(bikeId);
         return ResponseEntity.ok(bike);
@@ -58,17 +57,23 @@ public class BikeController {
     }
 
     @PostMapping("reserve/{bikeId}")
-    public ResponseEntity<Bike> reserveBike(@PathVariable String bikeId){
-       // bike bike = bikeService.reserveBike(bikeId);
-        //return ResponseEntity.ok(bike);
-         try {
-        // Call the service method to reserve the bike
-        Bike bike = bikeService.reserveBike(bikeId);
-        return ResponseEntity.ok(bike); // Return 200 OK with the bike details
-    } catch (IllegalArgumentException ex) {
-        // Handle exceptions and return 400 Bad Request with the error message
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+    public ResponseEntity<Bike> reserveBike(@PathVariable String bikeId) {
+        // bike bike = bikeService.reserveBike(bikeId);
+        // return ResponseEntity.ok(bike);
+        try {
+            // Call the service method to reserve the bike
+            Bike bike = bikeService.reserveBike(bikeId);
+            return ResponseEntity.ok(bike); // Return 200 OK with the bike details
+        } catch (IllegalArgumentException ex) {
+            // Handle exceptions and return 400 Bad Request with the error message
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
+
+    @PostMapping("return/{bikeId}")
+    public ResponseEntity<Bike> returnBike(@PathVariable String bikeId) {
+        Bike bike = bikeService.returnBike(bikeId);
+        return ResponseEntity.ok(bike);
     }
 
     @GetMapping("/isAvailable/{isAvailable}")
@@ -78,9 +83,10 @@ public class BikeController {
     }
 
     @PutMapping("/updateBikeStation/{bikeId}/{oldStationId}/{newStationId}")
-    public ResponseEntity<Bike> updateBikeStation(@PathVariable String bikeId, @PathVariable String oldStationId, @PathVariable String newStationId) {
+    public ResponseEntity<Bike> updateBikeStation(@PathVariable String bikeId, @PathVariable String oldStationId,
+            @PathVariable String newStationId) {
         Bike bike = bikeService.updateBikeStation(bikeId, oldStationId, newStationId);
         return ResponseEntity.ok(bike);
     }
-    
+
 }
