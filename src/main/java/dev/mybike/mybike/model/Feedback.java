a054f17023f0b6f37752c5e14a79f814c1d50aae
Sulakshika-Gunasekara <161403829+Sulakshika-Gunasekara@users.noreply.github.com>
2025-01-02 07:06:20 +0530

    Add new service Feedback

---
+++ src/main/java/dev/mybike/mybike/model/Feedback.java
@@ -0,0 +1,19 @@
+package dev.mybike.mybike.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document(collection = "feedback")
+public class Feedback {
+
+    @Id
+    private String id;
+    private String customerId;
+    private String feedback;
+    private int rating;
+    private String date;
+
+}
