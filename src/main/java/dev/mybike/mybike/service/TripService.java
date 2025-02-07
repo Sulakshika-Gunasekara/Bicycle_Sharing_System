f41bbb71c94537fee29c31ebd3df08000fb0cd22
Sulakshika-Gunasekara <161403829+Sulakshika-Gunasekara@users.noreply.github.com>
2025-02-07 14:46:56 +0530

    Implement new api end points as frontend needs

--- src/main/java/dev/mybike/mybike/service/TripService.java
+++ src/main/java/dev/mybike/mybike/service/TripService.java
@@ -1,7 +1,5 @@
 package dev.mybike.mybike.service;
 
-import java.util.Date;
-
 import dev.mybike.mybike.model.Rider;
 import dev.mybike.mybike.model.Trip;
 
@@ -9,16 +7,28 @@ public interface TripService {
 
     Trip getTripById(String tripId);
 
-    double calculatePayment(Trip trip, Rider rider);
-
     double addFunds(Rider rider, double amount);
 
     double deductFunds(Rider rider, double amount);
 
     double getBalance(Rider rider);
 
-    Trip startTrip(String riderId);
+    // get time duration of trip as data and calculate the payment amount based on
+    // duration of trip
+    // after a trip this will show the the amount to be paid
+    double calculatePayment(String tripID, String timeDuration); // add time duration as a string parameter
 
-    Trip endTrip(String tripId);
+    // update riders wallet balance
+    // return updated wallet balance
+    // get the rider and the amount of the last ended trip which return from
+    // calculatePayment
+    // update the wallet balance of the rider by reducing the amount of the last
+    // trip
+
+    double updateWalletBalance(Rider rider, double amount);
+
+    // add a new trip to the database
+    // return the trip
+    Trip startTrip(Rider rider, String bikeId);
 
 }
--- src/main/java/dev/mybike/mybike/service/impl/BikeServiceImpl.java
+++ src/main/java/dev/mybike/mybike/service/impl/BikeServiceImpl.java
@@ -1,5 +1,6 @@
 package dev.mybike.mybike.service.impl;
 
+import java.time.Instant;
 import java.util.Date;
 import java.util.List;
 
@@ -37,9 +38,6 @@ public class BikeServiceImpl implements BikeService {
         @Autowired
         private DockingStationRepository dockingStationRepository;
 
-        @Autowired
-        private TripRepository tripRepository;
-
         @Override
         public List<Bike> getAllBikes() {
                 return bikeRepository.findAll();
@@ -68,39 +66,7 @@ public class BikeServiceImpl implements BikeService {
                 return bikeRepository.save(bike);
         }
 
-        // @Override
-        // public Bike reserveBike(String bikeId, String oldStationId) {
-        // Bike bike = bikeRepository.findById(bikeId)
-        // .orElseThrow(() -> new IllegalArgumentException("Bike not found."));
-        // if (!bike.isAvailable()) {
-        // throw new IllegalArgumentException("Bike is not available.");
-        // }
-        // bike.setAvailable(false);
-        // // get Date and time
-        // // set start time
-        // Trip trip = tripRepository.findById(bikeId)
-        // .orElseThrow(() -> new IllegalArgumentException("Trip not found."));
-        // Date currentTime = new Date(System.currentTimeMillis());
-        // trip.setStartTime(currentTime);
-        // tripRepository.save(trip);
-
-        // // update new staiton available bikes
-        // // update old station empty docks
-        // // update new station empty docks
-        // // update old station available bikes
-        // // update Bike station id
-        // DockingStation oldDockingStation =
-        // dockingStationRepository.findById(oldStationId)
-        // .orElseThrow(() -> new RuntimeException("Old Docking Station not found."));
-
-        // oldDockingStation.setEmptyDocks(oldDockingStation.getEmptyDocks() + 1);
-        // oldDockingStation.setAvailableBikes(oldDockingStation.getAvailableBikes() -
-        // 1);
-        // dockingStationRepository.save(oldDockingStation);
-
-        // return bikeRepository.save(bike);
-        // }
-
+        @Transactional
         @Override
         public Bike reserveBike(String bikeId) {
                 Bike bike = bikeRepository.findById(bikeId)
@@ -116,44 +82,13 @@ public class BikeServiceImpl implements BikeService {
                                 .orElseThrow(() -> new RuntimeException("Old Docking Station not found."));
 
                 oldDockingStation.setEmptyDocks(oldDockingStation.getEmptyDocks() + 1);
-                oldDockingStation.setAvailableBikes(oldDockingStation.getAvailableBikes() - 1);
+                oldDockingStation.setAvailableBikes(oldDockingStation.getAvailableBikes() -
+                                1);
                 dockingStationRepository.save(oldDockingStation);
                 return bikeRepository.save(bike);
         }
 
-        // @Override
-        // public Bike returnBike(String bikeId, String newStationId) {
-        // Bike bike = bikeRepository.findById(bikeId)
-        // .orElseThrow(() -> new IllegalArgumentException("Bike not found."));
-        // if (bike.isAvailable()) {
-        // throw new IllegalArgumentException("Bike is already available.");
-        // }
-        // bike.setAvailable(true);
-        // // get Date and time
-        // // set return time
-        // Trip trip = tripRepository.findById(bikeId)
-        // .orElseThrow(() -> new IllegalArgumentException("Trip not found."));
-        // trip.setEndTime(new Date(System.currentTimeMillis()));
-        // tripRepository.save(trip);
-
-        // // update new staiton available bikes
-        // // update old station empty docks
-        // // update new station empty docks
-        // // update old station available bikes
-        // // update Bike station id
-        // DockingStation newDockingStation =
-        // dockingStationRepository.findById(newStationId)
-        // .orElseThrow(() -> new RuntimeException("New Docking Station not found."));
-        // newDockingStation.setEmptyDocks(newDockingStation.getEmptyDocks() - 1);
-        // newDockingStation.setAvailableBikes(newDockingStation.getAvailableBikes() +
-        // 1);
-        // dockingStationRepository.save(newDockingStation);
-
-        // bike.setStationId(newStationId);
-
-        // return bikeRepository.save(bike);
-        // }
-
+        @Transactional
         @Override
         public Bike returnBike(String bikeId, String newStationId) {
                 Bike bike = bikeRepository.findById(bikeId)
@@ -169,7 +104,8 @@ public class BikeServiceImpl implements BikeService {
                 DockingStation newDockingStation = dockingStationRepository.findById(newStationId)
                                 .orElseThrow(() -> new RuntimeException("New Docking Station not found."));
                 newDockingStation.setEmptyDocks(newDockingStation.getEmptyDocks() - 1);
-                newDockingStation.setAvailableBikes(newDockingStation.getAvailableBikes() + 1);
+                newDockingStation.setAvailableBikes(newDockingStation.getAvailableBikes() +
+                                1);
                 dockingStationRepository.save(newDockingStation);
 
                 bike.setStationId(newStationId);
@@ -212,7 +148,6 @@ public class BikeServiceImpl implements BikeService {
 
         @Override
         public List<Bike> getBikeByStationId(String stationId) {
-                // TODO Auto-generated method stub
                 return bikeRepository.findByStationId(stationId);
         }
 
