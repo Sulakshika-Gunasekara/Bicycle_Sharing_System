cccd88956d51d0367c1ad074727d547240ea5e3f
Himal-Gunawardhana <gunawar-ec20005@stu.kln.ac.lk>
2025-02-07 22:10:08 +0530

    rider-data endpoint added

--- src/main/java/dev/mybike/mybike/MyBikeApplication.java
+++ src/main/java/dev/mybike/mybike/MyBikeApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyBikeApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(MyBikeApplication.class, args);
     }
